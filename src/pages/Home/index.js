304f739a664a112974ca93dd05db0542ea870fd4
Cleriston-cmd <c.andrade6533@gmail.com>
2024-11-04 10:17:06 -0300

    Estilizando descrição de repositórios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import {Header} from "../../components/Header";
 import background from "../../assets/background.png";
+import ItemList from "../../components/ItemList";
 
 import "./styles.css";
 
@@ -27,6 +28,12 @@ function App() {
             </div>
           </div>
           <hr />
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title="Teste1" description="teste de descrição" />
+            <ItemList title="Teste1" description="teste de descrição" />
+            <ItemList title="Teste1" description="teste de descrição" />
+          </div>
         </div>
       </div>
     </div>
